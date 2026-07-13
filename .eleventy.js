1a37184f3611387764bded14241636de989a5bd0
Gariito <urangagames@gmail.com>
2026-07-13 21:07:06 +0200

    añadir favicon calculadora

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("src/android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
 
   eleventyConfig.addFilter("fecha", (dateObj) => {
-    return new Date(dateObj).toLocaleDateString("es-ES", { year: "numeric", month: "long", day: "numeric" });
+    return new Date(dateObj).toLocaleDateString("es-ES", {
+      year: "numeric", month: "long", day: "numeric"
+    });
   });
 
   return {
